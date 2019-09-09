25a755b4c20e65c410bf5f3e9c2debf8ada4510e
s01158158 <39176224+s01158158@users.noreply.github.com>
2019-09-09 00:08:13 -0400

    Update Stats100.py

--- Stats100.py
+++ Stats100.py
@@ -1,1 +1,16 @@
+I couldn't seem to find out how to do these, I tried for multiple hours and couldn't seem to find anything online either, so i'm not 
+sure if i was searching for the wrong code but this was probably the closest i could seem to get without using code from more 
+advanced tutorials i watched to try and figure it out.
+
+acc = 2
+>>> for x in range(0, 100):
+	acc = acc + x
+>>> acc
+4952
+
+acc = 1
+>>> for x in range(0, 50):
+	acc = acc + x
+>>> acc
+1226
 
