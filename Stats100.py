f99c7eb91976e3e50d91aecd76ab1735e3d2d907
s01158158 <39176224+s01158158@users.noreply.github.com>
2019-09-09 22:16:30 -0400

    Update Stats100.py

--- Stats100.py
+++ Stats100.py
@@ -2,15 +2,26 @@ I couldn't seem to find out how to do these, I tried for multiple hours and coul
 sure if i was searching for the wrong code but this was probably the closest i could seem to get without using code from more 
 advanced tutorials i watched to try and figure it out.
 
-acc = 2
->>> for x in range(0, 100):
-	acc = acc + x
->>> acc
-4952
+Nevermind I'm not sure why i couldn't find it anywhere online the past couple of days, but thank you for explaining it really well in 
+class, the book doesn't have simple explainations with examples I understand the lesson now.
 
-acc = 1
->>> for x in range(0, 50):
-	acc = acc + x
->>> acc
-1226
+x = 0
+for First100EvenSum in range(0, 201, 2):
+	x = x + First100EvenSum
+print(x)
+10100
 
+n = 0
+for First50OddSum in range(1, 50, 2):
+	n = n + First50OddSum
+print(n)
+625
+
+i = 0
+for First100Avg in range(1, 100, 2):
+	i = i + First100Avg
+	
+i // 100
+
+print(i)
+2500
